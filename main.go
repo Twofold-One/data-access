7629b500f2582fc838d43607d27fd328e8444948
Twofold-One <evan_geras@mail.ru>
2022-02-05 22:58:26 +0700

    basic project is ready

--- main.go
+++ main.go
@@ -53,9 +53,26 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("Albums found: %v\n", albums)
+	fmt.Printf("Albums found by name: %v\n", albums)
+
+	singleAlbum, err := albumByID(2)
+	if err != nil {
+		log.Fatal(err)
+	} 
+	fmt.Printf("Album found by id: %v\n", singleAlbum)
+
+	albumID, albumTitle, err := addAlbum(Album{
+		Title: "Reign in Blood",
+		Artist: "Slayer",
+		Price: 50.99,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("ID and title of added album: %v, %v\n", albumID, albumTitle)
 }
 
+// allAlbums queries for all available albums.
 func allAlbums() ([]Album, error) {
 	var albums []Album
 
@@ -78,6 +95,7 @@ func allAlbums() ([]Album, error) {
 	return albums, nil
 }
 
+// albumsByArtist queries for albums that have the specified artist name.
 func albumsByArtist(name string) ([]Album, error) {
 	var albums []Album
 
@@ -98,6 +116,31 @@ func albumsByArtist(name string) ([]Album, error) {
 		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
 	}
 	return albums, nil
+}
+
+// albumByID queries for the album with the specified ID.
+func albumByID(id int64) (Album, error) {
+	var alb Album
+
+	row := db.QueryRow("SELECT * FROM album WHERE id = $1", id)
+	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+		if err == sql.ErrNoRows {
+			return alb, fmt.Errorf("albumsById %d: no such album", id)
+		}
+		return alb, fmt.Errorf("albumsById %d: %v", id, err)
+	}
+	return alb, nil
+}
+
+// addAlbum adds the specified album to the database,
+// returning the album ID and Title of the new entry
+func addAlbum(alb Album) (int64, string, error) {
+	result, err := db.Exec("INSERT INTO album (title, artist, price) VALUES ($1, $2, $3) RETURNING id", alb.Title, alb.Artist, alb.Price)
+	if err != nil {
+		return 0, "", fmt.Errorf("addAlbum: %v", err)
+	}
+	fmt.Println(result)
+	return alb.ID, alb.Title, nil
 }
 
 
